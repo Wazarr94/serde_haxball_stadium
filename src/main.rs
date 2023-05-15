b41ea0e7e61c092b72f395ebcee34da0e8b3b5ac
Wazarr <jeje_04@live.com>
2023-05-15 23:30:56 +0200

    allow for comments in map

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::stadium::StadiumRaw;
+use jsonc_parser::parse_to_serde_value;
 use std::error::Error;
 use std::fs;
 
@@ -15,7 +16,8 @@ pub mod vertex;
 fn main() -> Result<(), Box<dyn Error>> {
     for stadium_file in fs::read_dir("stadiums")? {
         let stadium_str = fs::read_to_string(stadium_file?.path())?;
-        let stadium_raw: StadiumRaw = serde_json::from_str(&stadium_str)?;
+        let stadium_value = parse_to_serde_value(&stadium_str, &Default::default())?.unwrap();
+        let stadium_raw: StadiumRaw = serde_json::from_value(stadium_value)?;
         let stadium = stadium_raw.to_stadium();
         println!("Successfully read {}", &stadium.name);
     }
